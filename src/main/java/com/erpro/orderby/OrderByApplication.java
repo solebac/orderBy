a199e1d197dc7c3bd40c840ae70ca4431ea6c04d
Flavio Rogerio <solebac@hotmail.com>
2020-10-22 08:41:43 -0300

    Project Created

---
+++ src/main/java/com/erpro/orderby/OrderByApplication.java
@@ -0,0 +1,13 @@
+package com.erpro.orderby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderByApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderByApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erpro/orderby/OrderByApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erpro.orderby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderByApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3c23d5aefaed8227e7d2a3968ca84530f74aeb9b
Flavio Rogerio <solebac@hotmail.com>
2020-10-23 01:43:36 -0300

    Service layer, component registration

--- src/main/java/com/erpro/orderby/repositories/UserRepository.java
+++ src/main/java/com/erpro/orderby/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.erpro.orderby.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.erpro.orderby.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 }
---
+++ src/main/java/com/erpro/orderby/service/UserService.java
@@ -0,0 +1,25 @@
+package com.erpro.orderby.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.erpro.orderby.entities.User;
+import com.erpro.orderby.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> finAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}

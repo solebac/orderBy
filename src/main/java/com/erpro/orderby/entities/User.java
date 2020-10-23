ebf40e9bf0c4f1f2609818222eb12c09fe278ced
Flavio Rogerio <solebac@hotmail.com>
2020-10-23 03:02:21 -0300

    Entity Order Part-01

--- src/main/java/com/erpro/orderby/entities/User.java
+++ src/main/java/com/erpro/orderby/entities/User.java
@@ -1,15 +1,18 @@
 package com.erpro.orderby.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.hibernate.annotations.GeneratorType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_users")
 public class User implements Serializable {
 
 	/**
@@ -23,6 +26,12 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	//Um User tem varios Order
+	//Obs.: Deve-se instancia as Collections
+	//     -only Get, pois so vamos acrescentas e remover elementos
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -34,6 +43,10 @@ public class User implements Serializable {
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+	}
+		
+	public List<Order> getOrders() {
+		return orders;
 	}
 	public Long getId() {
 		return id;

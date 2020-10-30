5ac678a97424b5f94952ac722fe26b62fbe2cb65
Flavio Rogerio <solebac@hotmail.com>
2020-10-30 03:40:31 -0300

    Exception handling - delete

---
+++ src/main/java/com/erpro/orderby/resources/exceptions/DatabaseException.java
@@ -0,0 +1,16 @@
+package com.erpro.orderby.resources.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+		
+	}
+	
+
+}
